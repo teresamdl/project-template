14de4783181864015c55e052798af954055bb424
teresamdl <teresamdl@gmail.com>
2015-12-27 23:47:17 +0000

    Versao2.0

--- api/src/resources/hello.js
+++ api/src/resources/hello.js
@@ -9,3 +9,23 @@ exports.get = function handler (request, reply) {
 exports.post = function handler (request, reply) {
   reply(bl.sayHello(request.payload.name))
 }
+
+exports.getPatients = function handler (request, reply) {
+  reply(bl.getPatients())
+}
+
+exports.getRequests = function handler (request, reply) {
+  reply(bl.getRequests())
+}
+
+exports.postLogIn = function handler (request, reply) {
+  reply(bl.postLogIn(request.payload.user, request.payload.pass))
+}
+
+exports.getActs = function handler (request, reply) {
+  reply(bl.getActs())
+}
+
+exports.getReimb = function handler (request, reply) {
+  reply(bl.getReimb())
+}
